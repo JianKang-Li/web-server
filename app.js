e0dbdecaa691bd28f00e91388b6083ec0b548fc4
ljk <3104653373@qq.com>
2022-07-16 20:28:19 +0800

    init

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var os = require("os");
+var networkInterfaces = os.networkInterfaces();
+
+var app = express();
+app.engine("html", require("express-art-template"));
+app.use("/public", express.static("public"));
+app.use("/file", express.static("./public/files/"));
+// app.use("/node_modules", express.static("node_modules"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var Router = require("./routers/router");
+app.use("/", Router);
+
+app.use((err, req, res, next) => {
+  if (err) console.log(err);
+  res.send("服务器出错了！");
+  next();
+});
+
+// console.info(networkInterfaces);
+var getIPAddress = function () {
+  var ipv4 = "";
+  var ifaces = os.networkInterfaces();
+  // console.log(ifaces); //所有类型的适配器和全部内容
+  for (var dev in ifaces) {
+    ifaces[dev].forEach(function (details, alias) {
+      if (dev === "WLAN") {
+        //判断需要获取IP的适配器
+        if (details.family == "IPv4") {
+          //判断是IPV4还是IPV6 还可以通过alias去判断
+          ipv4 = details.address; //取addressIP地址
+        }
+      }
+      if (dev === "wlan0") {
+        if (details.family == "IPv4") {
+          ipv4 = details.address;
+        }
+      }
+    });
+  }
+  // console.log(ipv4);
+  return ipv4 || "127.0.0.1";
+};
+var ip = getIPAddress();
+app.listen(8888, function () {
+  console.log(`启动成功!
+- http://127.0.0.1:8888
+- http://${ip}:8888`);
+});
---
+++ routers/router.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var router = express.Router();
+var multiparty = require("multiparty");
+var fs = require("fs");
+
+router.get("/", function (req, res) {
+  res.render("index.html");
+});
+
+router.get("/menu", function (req, res) {
+  res.set("Access-Control-Allow-Origin", "*");
+  let menu = fs.readdirSync("./public/files/");
+  // console.log(menu);
+  let menu1 = [];
+  menu.forEach((item) => {
+    if (item != ".gitkeep") {
+      menu1.push(item);
+    }
+  });
+  res.send(menu1);
+});
+router.post("/upload", function (req, res) {
+  var form = new multiparty.Form();
+  form.uploadDir = "./files/";
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      console.log(err);
+    } else {
+      // console.log(files.file);
+      let name = files.file[0].originalFilename;
+      fs.rename(files.file[0].path, "./public/files/" + name, (err) => {
+        if (err) {
+          // console.log("重命名失败");
+          res.setHeader("Content-Type", "application/text");
+          res.send("上传失败");
+        } else {
+          // console.log("命名成功");
+          res.setHeader("Content-Type", "application/text");
+          res.send("上传成功");
+        }
+      });
+    }
+  });
+});
+
+router.get("/delete", function (req, res) {
+  arr = decodeURI(req.url).split("?");
+  filename = arr[1].split("=")[1];
+  fs.access("./public/files/" + filename, (err) => {
+    console.log(`${filename} ${err ? "does not exist" : "exists"}`);
+  });
+  try {
+    fs.unlinkSync("./public/files/" + filename);
+    res.send("文件已删除");
+  } catch (e) {
+    console.log(e);
+    res.send("删除失败");
+  }
+});
+module.exports = router;
